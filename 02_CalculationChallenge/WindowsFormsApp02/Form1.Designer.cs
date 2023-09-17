6ee6dbc6b30298736545a3c8148c364b0a9d06ee
H1MERU <1359248312@qq.com>
2023-09-17 15:09:28 +0800

    first work commit

---
+++ 02_CalculationChallenge/WindowsFormsApp02/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace WindowsFormsApp02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.qBox = new System.Windows.Forms.TextBox();
+            this.aBox = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(441, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 20F);
+            this.label1.Location = new System.Drawing.Point(62, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 34);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "最强小脑";
+            // 
+            // qBox
+            // 
+            this.qBox.Location = new System.Drawing.Point(68, 109);
+            this.qBox.Name = "qBox";
+            this.qBox.ReadOnly = true;
+            this.qBox.Size = new System.Drawing.Size(320, 25);
+            this.qBox.TabIndex = 2;
+            // 
+            // aBox
+            // 
+            this.aBox.Location = new System.Drawing.Point(68, 188);
+            this.aBox.Name = "aBox";
+            this.aBox.Size = new System.Drawing.Size(100, 25);
+            this.aBox.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(174, 190);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "确定";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(458, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "计时";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 277);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.aBox);
+            this.Controls.Add(this.qBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox qBox;
+        private System.Windows.Forms.TextBox aBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ 02_CalculationChallenge/WindowsFormsApp02/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp02
+{
+    public partial class Form1 : Form
+    {
+        private Random r = new Random();
+        private int qNum = 10;
+        private int curQuizNum = 0;
+        private int score  = 0;
+        private int qTime = 20;
+        private int curAns = 0;
+        private DateTime startTime;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartNewQuiz()
+        {
+            curQuizNum = 0;
+            score = 0;
+            button1.Text = "停止";
+            ContinueQuiz();
+        }
+
+        private void ContinueQuiz()
+        {
+            curQuizNum++;
+            aBox.BackColor = Color.White;
+            if (curQuizNum <= qNum)
+            {
+                var first = r.Next(1, 999);
+                var second = r.Next(1, 999);
+                var op = r.Next(0, 1);
+
+                if (op == 0)
+                    curAns = first + second;
+                if (op == 1)
+                    curAns = first - second;
+
+                var oper = op == 0 ? "+" : "-";
+
+                qBox.Text = $"{first} {oper} {second} = ?";
+                label2.Text = $"{qTime} s";
+                startTime = DateTime.Now;
+                timer1.Start();
+            }
+            else
+            {
+                EndQuiz();
+            }
+        }
+
+        private void EndQuiz()
+        {
+            timer1.Stop();
+            button1.Text = "开始";
+            MessageBox.Show($"总得分: {score}");
+        }
+
+        private void CheckAnswer()
+        {
+            int.TryParse(aBox.Text, out int userAnswer);
+
+            if (userAnswer == curAns)
+            {
+                aBox.BackColor = Color.Green;
+                score++;
+                Thread.Sleep(1000);
+                ContinueQuiz();
+            }
+            else
+            {
+                aBox.BackColor = Color.Red;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)//此函数是所有按键被按下时的相应函数
+
+        {
+
+            if (e.KeyCode == Keys.Enter)//判断回车键
+            {
+                this.button2_Click(sender, e);//触发按钮事件
+
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "开始")
+                StartNewQuiz();
+            else
+                EndQuiz();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CheckAnswer();
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            TimeSpan elapsed = DateTime.Now - startTime;
+            int remainingSeconds = qTime - (int)elapsed.TotalSeconds;
+            label2.Text = $"{remainingSeconds} s";
+
+            if (remainingSeconds <= 0)
+            {
+                timer1.Stop();
+                ContinueQuiz() ;
+            }
+        }
+    }
+}
