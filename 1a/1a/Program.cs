78444c68b58bd02e31bad0cd6cc86fd80e5fd09c
H1MERU <1359248312@qq.com>
2023-09-18 19:38:45 +0800

    first homework commit: 1a changed

---
+++ 1a/1a/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("分解质因数");
+        Console.WriteLine("输入数字：");
+        string num = Console.ReadLine();
+        if (Regex.IsMatch(num, @"^[0-9]+$"))
+        {
+            var n = Convert.ToInt32(num);
+            Console.WriteLine(Fractorization.fraction(n));
+        }
+        else
+        {
+            Console.WriteLine("请输入正确的数字");
+        }
+    }
+
+    public partial class Fractorization
+    {
+        public static string fraction(int num)
+        {
+            string str = num.ToString() + " = ";
+
+            int n = num;
+            int i = 2;
+
+            while (i <= n)
+            {
+                if (n % i == 0)
+                {
+                    if (i == n)
+                        str += i.ToString();
+                    else
+                        str += i.ToString() + " * ";
+                    n = n / i;
+                    continue;
+                }
+
+                i++;
+            }
+
+            return str;
+        }
+    }
+}
