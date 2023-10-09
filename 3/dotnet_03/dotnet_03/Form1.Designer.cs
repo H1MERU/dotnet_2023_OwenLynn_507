8c81eb6396f0cff177d2589aa146263f46991b89
H1MERU <1359248312@qq.com>
2023-10-09 23:55:04 +0800

    第三次作业提交

---
+++ 3/dotnet_03/dotnet_03/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace dotnet_03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.selectFileButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.form1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // selectFileButton
+            // 
+            this.selectFileButton.Location = new System.Drawing.Point(615, 50);
+            this.selectFileButton.Name = "selectFileButton";
+            this.selectFileButton.Size = new System.Drawing.Size(75, 23);
+            this.selectFileButton.TabIndex = 0;
+            this.selectFileButton.Text = "选择文件";
+            this.selectFileButton.UseVisualStyleBackColor = true;
+            this.selectFileButton.Click += new System.EventHandler(this.SelectFileButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(92, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "选择的文件";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(98, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "格式化前：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(98, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "格式化后：";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(615, 116);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "格式化";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(101, 199);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // form1BindingSource
+            // 
+            this.form1BindingSource.DataSource = typeof(dotnet_03.Form1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.selectFileButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button selectFileButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource form1BindingSource;
+    }
+}
+
---
+++ 3/dotnet_03/dotnet_03/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace dotnet_03
+{
+    public partial class Form1 : Form
+    {
+        public string fileName;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            Console.WriteLine(fileName);
+        }
+
+        private void SelectFileButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = false;//该值确定是否可以选择多个文件
+            dialog.Title = "请选择文件夹";
+            dialog.Filter = "C#文件(*.cs)|*.cs";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                fileName = dialog.FileName;
+                label1.Text = dialog.FileName;
+            }
+        }
+
+        private void FirstSummerize()
+        {
+            using StreamReader sr = File.OpenText(fileName);
+            using StreamWriter sw = File.CreateText(fileName + "_formulated.cs") ;
+            int lineCount = 0;
+            int wordCount = 0;
+            for(string s = sr.ReadLine(); s!=null; s = sr.ReadLine())
+            {
+                lineCount++;
+                s = DeleteComment(s);
+                if(s != "")
+                {
+                    sw.WriteLine(s);
+                }
+                wordCount += CountWords(s);
+            }
+            label2.Text = "格式化前有" + lineCount + "行，" + wordCount + "字。";
+            sw.Dispose();
+            SecondSummerize();
+        }
+
+        private void SecondSummerize()
+        {
+            using StreamReader srw = File.OpenText(fileName + "_formulated.cs");
+            int lineCount = 0;
+            int wordCount = 0;
+            Dictionary<string, int> wordCounts = new Dictionary<string, int>();
+            for (string s = srw.ReadLine(); s != null; s = srw.ReadLine())
+            {
+                lineCount++;
+                wordCount += CountWords(s);
+                string[] words = s.Split(new char[] { ' ', '\t', '\r', '\n', '.', ',', ';', ':', '(', ')', '{', '}', '[', ']', '=', '+', '-', '*', '/', '<', '>', '&', '|', '!', '?', '\"', '\'', '\\' }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string word in words)
+                {
+                    string cleanedWord = word.Trim();
+                    if (!string.IsNullOrWhiteSpace(cleanedWord))
+                    {
+                        if (wordCounts.ContainsKey(cleanedWord))
+                        {
+                            wordCounts[cleanedWord]++;
+                        }
+                        else
+                        {
+                            wordCounts.Add(cleanedWord, 1);
+                        }
+                    }
+                }
+            }
+            dataGridView1.Columns.Add("Word", "单词");
+            dataGridView1.Columns.Add("Count", "出现次数");
+            foreach (var kvp in wordCounts.OrderByDescending(kv => kv.Value))
+                {
+                    dataGridView1.Rows.Add(kvp.Key, kvp.Value);
+                }
+            label3.Text = "格式化后有" + lineCount + "行，" + wordCount + "字。";
+        }
+
+
+        private static int CountWords(string text)
+        {
+            string pattern = @"\b\w+\b";
+            MatchCollection matches = Regex.Matches(text, pattern);
+            return matches.Count;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        public string DeleteComment(string s)
+        {
+            int i;
+            for(i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '/')
+                {
+                    break;
+                }
+            }
+            return s.Substring(0, i);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FirstSummerize();
+        }
+    }
+}
